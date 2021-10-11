ebe66c00b9acad4b8c0851c3e2879d455d1c124f
iamoshiro <ricardo.osh@gmail.com>
2021-10-11 09:24:58 -0300

    Update bug001.sh

--- bug001.sh
+++ bug001.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Apagar os arquivos criados pelo tightvncserver no diretório /tmp
+# Delete the files created by the tightvnc server in the /tmp directory
 rm -rf /tmp/dbus-* /tmp/pulse-*/ /tmp/ssh-* /tmp/.ICE-unix/ /tmp/.X11-unix/ /tmp/.xfsm-ICE-* /tmp/.X0-lock
 
-# Passar varíavel de usuário e executar o tightvncserver
+# Export user variable and run tightvncserver
 export USER=root
 tightvncserver :0 -geometry 1280x800 -depth 16 -pixelformat rgb565
 
-# Descomente a linha abaixo caso utilize o novnc
+# Uncomment the line below if you use novnc
 #/usr/share/novnc/utils/launch.sh --listen 5901 --vnc localhost:5900
